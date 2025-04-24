3cc482b1b0e16c8351d16890cf35383b8e9f7bc9
Josh2004LC <mlsjos5577@gmail.com>
2025-04-23 20:29:32 -0600

    Se agrega controller de prestramos

--- Controllers/PrestamoController.cs
+++ Controllers/PrestamoController.cs
@@ -1,8 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BibliotecaAPI.Models;
+using BibliotecaAPI.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BibliotecaAPI.Controllers
 {
+
+    [ApiController]
+    [Route("api/[controller]")]
     public class PrestamoController : Controller
     {
+        private readonly PrestamoService _prestamoService;
+
+        public PrestamoController(PrestamoService prestamoService)
+        {
+            _prestamoService = prestamoService;
+        }
+
+        // GET: api/prestamo
+        [HttpGet]
+        public async Task<ActionResult<List<PrestamoModel>>> ObtenerPrestamos()
+        {
+            var prestamos = await _prestamoService.ObtenerPrestamosAsync();
+            return Ok(prestamos);
+        }
+
+        // POST: api/prestamo
+        [HttpPost]
+        public async Task<ActionResult> RegistrarPrestamo([FromBody] PrestamoModel prestamo)
+        {
+            await _prestamoService.RegistrarPrestamoAsync(prestamo);
+            return CreatedAtAction(nameof(ObtenerPrestamos), new { id = prestamo.id_prestamo }, prestamo);
+        }
+
+        // PUT: api/prestamo/{id}
+        [HttpPut("{id}")]
+        public async Task<ActionResult> ActualizarPrestamo(int id, [FromBody] PrestamoModel prestamo)
+        {
+            var actualizado = await _prestamoService.ActualizarPrestamoAsync(id, prestamo);
+            if (!actualizado) return NotFound();
+            return NoContent();
+        }
+
+        // DELETE: api/prestamo/{id}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> EliminarPrestamo(int id)
+        {
+            var eliminado = await _prestamoService.EliminarPrestamoAsync(id);
+            if (!eliminado) return NotFound();
+            return NoContent();
+        }
     }
 }
